b9c73c654fee3743286697a83128cebce44d2879
Ayman Shorafa <ayman.shurafa.99@gmail.com>
2026-08-12 11:42:06 +0300

    Add Data Provider for multiple login scenarios

--- automation/src/test/java/tests/LoginTest.java
+++ automation/src/test/java/tests/LoginTest.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+
 
 public class LoginTest {
 
@@ -35,8 +37,44 @@ public class LoginTest {
         Assert.assertTrue(loginPage.getErrorMessage()
             .contains("Username and password do not match"), "Error not shown!");
     }
+    
+    // Tracking the user in the Right page 
+    @Test
+    public void testSuccessfulLogin() {
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLogin();
 
-    @AfterMethod
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("inventory.html"), "Login failed: URL does not contain inventory.html");
+    }
+    
+    @DataProvider(name = "loginData")
+    public Object[][] loginData(){
+    	return new Object [][] {
+    		 {"standard_user", "secret_sauce", true},
+             {"locked_out_user", "secret_sauce", false},
+             {"wrong_user", "wrong_pass", false}
+    	};
+    }
+    
+    @Test(dataProvider = "loginData")
+    	public void testLoginWithMultipleUsers
+    	(String name , String pass , boolean shouldPass)
+    	{
+    	
+    	
+    	loginPage.login(name, pass);
+    	if(shouldPass) {
+    	Assert.assertTrue(driver.getCurrentUrl().contains("inventory")
+    			,"Login failed For Name : "+ name );
+    	}else {
+    		 Assert.assertFalse(driver.getCurrentUrl().contains("inventory"), 
+    		            "Should have failed for: " + name);
+    		}
+    	}
+    
+    	@AfterMethod
     public void teardown() {
         driver.quit();
     }
