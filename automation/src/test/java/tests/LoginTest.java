e797e1a549c46a347be20b33f29b3b472507622c
Ayman Shorafa <ayman.shurafa.99@gmail.com>
2026-08-12 13:06:58 +0300

    Add ScreenshotUtils for failed test capture

--- automation/src/test/java/tests/LoginTest.java
+++ automation/src/test/java/tests/LoginTest.java
@@ -4,12 +4,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
-
 
 public class LoginTest {
 
@@ -18,64 +18,62 @@ public class LoginTest {
 
     @BeforeMethod
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com");
-        loginPage = new LoginPage(driver);
+    	  WebDriverManager.chromedriver().setup();
+    	    driver = new ChromeDriver();
+    	    driver.manage().window().maximize();
+    	    driver.get("https://www.saucedemo.com");
+    	    driver.manage().deleteAllCookies(); // امسح الـ cookies
+    	    loginPage = new LoginPage(driver);
     }
 
     @Test
     public void testValidLogin() {
-        loginPage.login("standard_user", "secret_sauce");
+    	loginPage.login("standard_user", "secret_sauce");
         Assert.assertTrue(driver.getCurrentUrl().contains("inventory"), "Login failed!");
     }
-
     @Test
     public void testInvalidLogin() {
         loginPage.login("wrong_user", "wrong_pass");
         Assert.assertTrue(loginPage.getErrorMessage()
             .contains("Username and password do not match"), "Error not shown!");
     }
-    
-    // Tracking the user in the Right page 
+
     @Test
     public void testSuccessfulLogin() {
         loginPage.enterUsername("standard_user");
         loginPage.enterPassword("secret_sauce");
         loginPage.clickLogin();
-
         String currentUrl = driver.getCurrentUrl();
-        Assert.assertTrue(currentUrl.contains("inventory.html"), "Login failed: URL does not contain inventory.html");
+        Assert.assertTrue(currentUrl.contains("inventory.html"), 
+            "Login failed: URL does not contain inventory.html");
     }
-    
+
     @DataProvider(name = "loginData")
-    public Object[][] loginData(){
-    	return new Object [][] {
-    		 {"standard_user", "secret_sauce", true},
-             {"locked_out_user", "secret_sauce", false},
-             {"wrong_user", "wrong_pass", false}
+    public Object[][] loginData() {
+    	return new Object[][] {
+    		{"standard_user", "secret_sauce", true}, 
+    	    {"locked_out_user", "secret_sauce", false},
+    	    {"wrong_user", "wrong_pass", false}
     	};
     }
-    
+
     @Test(dataProvider = "loginData")
-    	public void testLoginWithMultipleUsers
-    	(String name , String pass , boolean shouldPass)
-    	{
-    	
-    	
-    	loginPage.login(name, pass);
-    	if(shouldPass) {
-    	Assert.assertTrue(driver.getCurrentUrl().contains("inventory")
-    			,"Login failed For Name : "+ name );
-    	}else {
-    		 Assert.assertFalse(driver.getCurrentUrl().contains("inventory"), 
-    		            "Should have failed for: " + name);
-    		}
-    	}
-    
-    	@AfterMethod
-    public void teardown() {
+    public void testLoginWithMultipleUsers(String name, String pass, boolean shouldPass) {
+        loginPage.login(name, pass);
+        if (shouldPass) {
+            Assert.assertTrue(driver.getCurrentUrl().contains("inventory"),
+                "Login failed for: " + name);
+        } else {
+            Assert.assertFalse(driver.getCurrentUrl().contains("inventory"),
+                "Should have failed for: " + name);
+        }
+    }
+
+    @AfterMethod
+    public void teardown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            utils.ScreenshotUtils.TakeScreenshot(driver, result.getName());
+        }
         driver.quit();
     }
 }
